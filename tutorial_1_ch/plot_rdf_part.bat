1111d6dd3c89f405dd9178898115bfb3c280a90c
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2019-12-06 21:34:31 +0900

    Delete plot_rdf_part.bat

--- tutorial_1_ch/plot_rdf_part.bat
+++
@@ -1,9 +0,0 @@
-echo set xlabel "Distance / nm" > plot.gp
-echo set xtics 0.1 >> plot.gp
-echo set mxtics 5 >> plot.gp
-echo set ylabel "G(r)" >> plot.gp
-echo set mytics 5 >> plot.gp
-echo plot "out_rdf.txt" u ($2/10):3 w l t "C-C", "out_rdf.txt" u ($2/10):5 w l t "H-H", "out_rdf.txt" u ($2/10):7 w l t "C-H" >> plot.gp
-echo pause -1  >> plot.gp
-gnuplot plot.gp
-del plot.gp
