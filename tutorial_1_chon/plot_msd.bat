33bef6dbdcda8f9bee3c3f6403ad78d70019e339
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2019-12-06 21:36:07 +0900

    Delete plot_msd.bat

--- tutorial_1_chon/plot_msd.bat
+++
@@ -1,9 +0,0 @@
-echo set xlabel "Time / ps" > plot.gp
-echo set xtics 0.1 >> plot.gp
-echo set mxtics 5 >> plot.gp
-echo set ylabel "r^2 / Angstrom^2" >> plot.gp
-echo set mytics 5 >> plot.gp
-echo plot "out_all_msd.txt" u ($1*0.5/1000):2 w l t "Total", "out_C_msd.txt" u ($1*0.5/1000):2 w l t "C", "out_H_msd.txt" u ($1*0.5/1000):2 w l t "H", "out_O_msd.txt" u ($1*0.5/1000):2 w l t "O", "out_N_msd.txt" u ($1*0.5/1000):2 w l t "N"  >> plot.gp
-echo pause -1  >> plot.gp
-gnuplot plot.gp
-del plot.gp
