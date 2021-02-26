9203879b53d01d33d1e8fe0bfb7b70f7e418eb7a
by-student-2017 <studentsctest@gmail.com>
2021-02-26 16:57:35 +0900

    windows10

---
+++ advance/ReaxFF-SiliconPolymers/2_mech_props/2_youngs_modulus/ppan/z/trial_4/run.bat
@@ -0,0 +1,15 @@
+nif exist cfg del cfg
+mkdir cfg
+
+set day=%date%
+set time=%time%
+echo "Job started on my PC at %day% %time%" 
+
+set SLURM_JOBID=%cd%
+echo "SLURM_JOBID=%SLURM_JOBID%"
+
+"C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.reaxc_y
+
+set day=%date%
+set time=%time%
+echo "Job finished on my PC at %day% %time%"  
---
+++ advance/ReaxFF-SiliconPolymers/README.md
@@ -0,0 +1,5 @@
+# ReaxFF-SiliconPolymers
+Code written by Manav Bhati, PhD Candidate, Chemical Engineering, Rice University, Houston, TX, 77005
+
+Distributed as part of the publication "Identifying Adhesion Properties at Si/Polymer Interfaces with ReaxFF"
+https://doi.org/10.1021/acs.jpcc.9b08216
