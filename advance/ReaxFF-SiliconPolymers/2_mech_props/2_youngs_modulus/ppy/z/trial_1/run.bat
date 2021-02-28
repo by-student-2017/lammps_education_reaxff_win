e72cfd2ff1200fc3da21e6701497f79d9828986e
by-student-2017 <studentsctest@gmail.com>
2021-02-28 11:12:59 +0900

    windows10

--- advance/ReaxFF-SiliconPolymers/2_mech_props/2_youngs_modulus/ppy/z/trial_1/run.bat
+++ advance/ReaxFF-SiliconPolymers/2_mech_props/2_youngs_modulus/ppy/z/trial_1/run.bat
@@ -8,7 +8,7 @@ echo "Job started on my PC at %day% %time%"
 set SLURM_JOBID=%cd%
 echo "SLURM_JOBID=%SLURM_JOBID%"
 
-"C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.reaxc_y
+"C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.reaxc_z
 
 set day=%date%
 set time=%time%
