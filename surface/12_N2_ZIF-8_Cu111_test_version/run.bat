932cfbe8bcef276ca6d216b04388d1475b6d4951
by-student-2017 <studentsctest@gmail.com>
2021-05-02 08:02:10 +0900

    windows 10

--- surface/12_N2_ZIF-8_Cu111_test_version/run.bat
+++ surface/12_N2_ZIF-8_Cu111_test_version/run.bat
@@ -1,4 +1,4 @@
 mkdir cfg
 
-"C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.N2_ZIF-8_Cu111
+"C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.N2_ZIF-8_Cu111_v2
 
