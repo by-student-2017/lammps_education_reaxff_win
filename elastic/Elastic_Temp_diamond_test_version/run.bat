2c20a5216e2d352a3be40b75b7ce3cd12e5a24b6
by-student-2017 <studentsctest@gmail.com>
2021-06-23 15:28:27 +0900

    windows 10

--- elastic/Elastic_Temp_diamond_test_version/run.bat
+++ elastic/Elastic_Temp_diamond_test_version/run.bat
@@ -2,6 +2,13 @@ REM mkdir cfg
 
 "C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.elastic
 
-python3 compliance_python3.py
+@echo off
+for /F "tokens=* USEBACKQ" %%F in (`python --version`) do (set var=%%F)
+if "%var:~0,8%"  == "Python 3" (
+  python compliance_python3.py > results.txt
+) else (
+  python3 compliance_python3.py > results.txt
+)
+type results.txt
 
 pause
