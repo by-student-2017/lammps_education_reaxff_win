2d2251e116e5015fe536ed35ddc24bb766360de1
by-student-2017 <studentsctest@gmail.com>
2023-05-22 13:08:38 +0900

    windows11

---
+++ elastic/BORN_MATRIX/Silicon_MSMPI_test_version_failed/run_msmpi.bat
@@ -0,0 +1,8 @@
+set ncore=1
+set OMP_NUM_THREADS=1
+
+REM mkdir cfg
+
+"C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in in.elastic
+
+pause
