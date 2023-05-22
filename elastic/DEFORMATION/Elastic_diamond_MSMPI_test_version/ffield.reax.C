4f0f89cc7a8061a1860102484ddd8f1667273f81
by-student-2017 <studentsctest@gmail.com>
2023-05-22 12:04:09 +0900

    windows11

---
+++ elastic/DEFORMATION/Elastic_diamond_MSMPI_test_version/ffield.reax.C
@@ -0,0 +1,59 @@
+Reactive MD-force field: Li et al. Comput. Mater. Sci. 169(2019) 109143
+ 39       ! Number of general parameters                                        
+    50.0000 !p(boc1)
+     9.5469 !p(boc2)
+    26.5405 !p(coa2)
+     1.7224 !p(trip4)
+     6.8702 !p(trip3)
+    70.0000 !kc2
+     1.0588 !p(ovun6)
+     4.6000 !p(trip2)
+    12.1176 !p(ovun7)
+    13.3056 !p(ovun8)
+   -13.3400 !p(trip1)
+     0.0000 !Lower Taper-radius (swa)
+    10.0000 !Upper Taper-radius (swb)
+     0.0000 !not used
+    33.8667 !p(val7)
+     6.0891 !p(lp1)
+     1.0563 !p(val9)
+     2.0384 !p(val10)
+     6.1431 !not used
+     6.9290 !p(pen2)
+     0.3989 !p(pen3)
+     3.9954 !p(pen4)
+     0.0000 !not used
+     5.7796 !p(tor2)
+    10.0000 !p(tor3)
+     1.9487 !p(tor4)
+     0.0000 !not used
+     2.1645 !p(cot2)
+     1.5591 !p(vdW1)
+     0.1000 !Cutoff for bond order*100 (cutoff)
+     2.1365 !p(coa4)
+     0.6991 !p(ovun4)
+    50.0000 !p(ovun3)
+     1.8512 !p(val8)
+     0.0000 !not used
+     0.0000 !not used
+     0.0000 !not used
+     0.0000 !not used
+     2.6962 !p(coa3)                                 
+ 1    ! Nr of atoms; cov.r; valency;a.m;Rvdw;Evdw;gammaEEM;cov.r2;#            
+            alfa;gammavdW;valency;Eunder;Eover;chiEEM;etaEEM;n.u.               
+            cov r3;Elp;Heat inc.;n.u.;n.u.;n.u.;n.u.                            
+            ov/un;val1;n.u.;val3,vval4                                         
+ C    1.3674   4.0000  12.0000   2.0453   0.1444   0.7920   1.1706   4.0000     
+      9.0000   1.5000   4.0000  27.5134  79.5548   6.7897   6.0000   0.0000     
+      1.1168   0.0000 181.0000  14.2732  24.4406   6.7313   0.8563   0.0000     
+     -4.1021   5.0000   1.0564   4.0000   2.9663   0.0000   0.0000   0.0000         
+ 1      ! Nr of bonds; Edis1;LPpen;n.u.;pbe1;pbo5;13corr;pbo6                  
+                         pbe2;pbo3;pbo4;n.u.;pbo1;pbo2;ovcorr
+  1  1  80.8865 107.9944  52.0636   0.5218  -0.3636   1.0000  34.9876   0.7769  
+         6.1244  -0.1693   8.0804   1.0000  -0.0586   8.1850   1.0000   0.0000
+ 0    ! Nr of off-diagonal terms; Ediss;Ro;gamma;rsigma;rpi;rpi2
+ 1    ! Nr of angles;at1;at2;at3;Thetao,o;ka;kb;pv1;pv2                        
+  1  1  1  74.9085  44.7514   0.9144   0.0000   0.0050   0.3556   2.5715 
+ 1    ! Nr of torsions;at1;at2;at3;at4;;V1;V2;V3;V2(BO);vconj;n.u;n            
+  1  1  1  1   2.1207  26.8713   0.5160  -9.0000  -2.8394   0.0000   0.0000  
+ 0    ! Nr of hydrogen bonds;at1;at2;at3;Rhb;Dehb;vhb1
