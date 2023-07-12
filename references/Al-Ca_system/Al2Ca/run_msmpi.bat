9d23ad94b4c32582925559b433e7f254ab12894e
by-student-2017 <studentsctest@gmail.com>
2023-07-12 09:55:06 +0900

    windows11

--- references/Al-Ca_system/Al2Ca/run_msmpi.bat
+++ references/Al-Ca_system/Al2Ca/run_msmpi.bat
@@ -1,6 +1,6 @@
 set ncore=4
 
-mkdir cfg
+#mkdir cfg
 
 for %%1 in (in.*) do (
   "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
