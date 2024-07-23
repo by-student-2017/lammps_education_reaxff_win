97e954cc8d9428dcf35b3577421ab104a6494600
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-07-23 17:51:42 +0900

    Add files via upload

---
+++ cluster/peptide_electric_field_reaxff_hybrid_ONIOM/run_msmpi_step1.bat
@@ -0,0 +1,4 @@
+
+set ncore=1
+
+"C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in in_step1.lmp
---
+++ cluster/peptide_electric_field_reaxff_hybrid_ONIOM/run_msmpi_step3.bat
@@ -0,0 +1,6 @@
+
+set ncore=1
+
+mkdir cfg
+
+"C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in in_step3.lmp
